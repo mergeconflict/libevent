bdba591e56e0a8218f9f66c1f8a8ad65137b1174
Dan Rosen <mergeconflict@google.com>
2019-03-24 19:55:05 -0400

    strengthen the test a bit with timing info

--- test/test-prepare-check.c
+++ test/test-prepare-check.c
@@ -33,15 +33,13 @@ static int prepare_callback_1_count = 0;
 static int prepare_callback_2_count = 0;
 static int check_callback_1_count = 0;
 static int check_callback_2_count = 0;
+static struct timeval start_time = { 0, 0 };
+static struct timeval end_time = { 0, 0 };
 
 #define log_callback \
 	printf("%s:\n", __func__); \
 	printf("  now: %ld.%06ld\n", info->now.tv_sec, info->now.tv_usec); \
-	if (info->timeout) { \
-		printf("  timeout: %ld.%06ld\n", info->timeout->tv_sec, info->timeout->tv_usec); \
-	} else { \
-		printf("  timeout: NULL\n"); \
-	}
+	printf("  timeout: %ld.%06ld\n", info->timeout->tv_sec, info->timeout->tv_usec);
 
 void
 prepare_callback_1(struct event_watcher *watcher, const struct event_watcher_cb_info *info)
@@ -53,6 +51,13 @@ prepare_callback_1(struct event_watcher *watcher, const struct event_watcher_cb_
 	assert(prepare_callback_1_count > prepare_callback_2_count);
 	assert(prepare_callback_1_count > check_callback_1_count);
 	assert(prepare_callback_1_count > check_callback_2_count);
+
+	/* if we've just scheduled the timeout event at the beginning of the iteration, save the current time and assert that the timeout is what we set */
+	if (start_time.tv_sec == 0) {
+		start_time = info->now;
+		assert(info->timeout->tv_sec == 1);
+		assert(info->timeout->tv_usec == 0);
+	}
 }
 
 void
@@ -75,6 +80,9 @@ check_callback_1(struct event_watcher *watcher, const struct event_watcher_cb_in
 
 	/* check_callback_1 should always fire before check_callback_2 */
 	assert(check_callback_1_count > check_callback_2_count);
+
+	/* save the end time, in case the timeout fires this time through the event loop */
+	end_time = info->now;
 }
 
 void
@@ -91,6 +99,9 @@ void
 timeout_callback(evutil_socket_t fd, short events, void *arg)
 {
 	printf("timeout_callback\n");
+
+	/* the duration between the start and end times should be at least 1 second */
+	assert(end_time.tv_sec >= start_time.tv_sec + 1);
 }
 
 int
@@ -105,6 +116,7 @@ main(int argc, char **argv)
 	struct event_watcher *check_callback_2_watcher = event_watcher_check_new(base, &check_callback_2);
 
 	/* schedule an 1 second timeout event, and run the event loop until the timeout fires */
+	printf("iteration 1\n-----------\n");
 	struct timeval tv;
 	tv.tv_sec = 1;
 	tv.tv_usec = 0;
@@ -112,7 +124,9 @@ main(int argc, char **argv)
 	event_base_dispatch(base);
 
 	/* second iteration: free two of the watchers, schedule a timeout and run the event loop again */
+	printf("iteration 2\n-----------\n");
 	iteration = 1;
+	start_time.tv_sec = 0;
 	event_watcher_free(prepare_callback_2_watcher);
 	event_watcher_free(check_callback_2_watcher);
 	event_base_once(base, -1, EV_TIMEOUT, &timeout_callback, 0, &tv);
